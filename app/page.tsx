573de6eb343a806bf7477b0fbe5ea1e386e1ec6d
Travis Falstad <tfalstad@gmail.com>
2024-07-16 15:56:02 -0500

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import MyComponent from '../components/MyComponent';
 
 export default function Home() {
     const imagePaths = {
-        background: 'images/background.png',
-        message: 'images/message.png',
-        meet: 'images/meet.png',
-        linkedin: 'images/linkedin.png',
-        portfolio: 'images/portfolio.png',
-        mixtaRe: 'images/mixta-re.png',
-        adaptableHuman: 'images/adaptable-human.png',
+        background: 'background.png',
+        message: 'message.png',
+        meet: 'meet.png',
+        linkedin: 'linkedin.png',
+        portfolio: 'portfolio.png',
+        mixtaRe: 'mixta-re.png',
+        adaptableHuman: 'adaptable-human.png',
     };
 
     return <MyComponent imagePaths={imagePaths} />;
